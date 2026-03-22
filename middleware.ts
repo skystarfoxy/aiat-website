524504db389add647773df11031791c3c0439664
skystarfoxy <adrian@active-medical.fr>
2026-03-22 03:40:40 +0200

    Debug: Temporarily disable middleware to troubleshoot 503 error

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
+/*
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
+*/
+
+export async function middleware() {
+  return
+}
+
 
 export const config = {
   matcher: [
